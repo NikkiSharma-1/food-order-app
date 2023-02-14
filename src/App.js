880807e69a03cc80f90af97be3d4b0e798f1ca72
NikkiSharma-1 <114333988+NikkiSharma-1@users.noreply.github.com>
2023-02-14 22:03:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Fragment } from "react";
 import Header from "./Components/Header/Header";
+import Summary from "./Components/Summary/Summary";
 function App() {
   return (
    <Fragment>
     <Header />
+    <main>
+    <Summary />
+    </main>
    </Fragment>
   );
 }
